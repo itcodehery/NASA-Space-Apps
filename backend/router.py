888457ea637ce8b9802a70ca3c64452233f15603
Darshan Heble <darshanheble@gmail.com>
2025-08-22 16:34:39 +0530

    removed lib and scripts

--- backend/router.py
+++ backend/router.py
@@ -1,39 +1,28 @@
-from fastapi import APIRouter , Query
+from fastapi import APIRouter, Query
 import pandas as pd
 import json
 import io
-from typing import Dict , List , Optional
+from typing import Dict, List, Optional
 from pathlib import Path
 from geopy.geocoders import Nominatim
 
 
-
 router = APIRouter()
-geolocator = Nominatim(user_agent = "geoapi")
+geolocator = Nominatim(user_agent="geoapi")
 
-df = pd.read_json('your_file.json')
-with open('your_file.json', 'r') as file:
-    data = json.load(file)
+# df = pd.read_json('your_file.json')
+# with open('your_file.json', 'r') as file:
+#     data = json.load(file)
 
-df = pd.DataFrame(data)
-
-
-
-
+# df = pd.DataFrame(data)
 
 
 @router.get("/get_country", response_model=Dict[str, str])
 async def get_country(latitude: float = Query(...), longitude: float = Query(...)):
-    location = geolocator.reverse((latitude, longitude), language='en')
-    if location and "country" in location.raw.get('address', {}):
-        country = location.raw['address']["country"]
+    location = geolocator.reverse((latitude, longitude), language="en")
+    if location and "country" in location.raw.get("address", {}):
+        country = location.raw["address"]["country"]
     else:
         country = "Not found"
 
-    return {
-        "latitude": str(latitude),
-        "longitude": str(longitude),
-        "country": country
-    }
-
-
+    return {"latitude": str(latitude), "longitude": str(longitude), "country": country}
