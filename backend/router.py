db347f23d0a66813da0f5a89399b40b302600504
ArdenDiago <diagoarden@gmail.com>
2025-08-22 22:40:54 +0530

    finally

--- backend/router.py
+++ backend/router.py
@@ -1,15 +1,18 @@
-#from fastapi import APIRouter , Query
+from fastapi import APIRouter , Query
 import pandas as pd
-from dataprocessing.Data.data import df
+import sys, os
+sys.path.append(os.path.dirname(__file__))  # add backend folder to path
+from dataProcessing.data import df
+
 router = APIRouter()
 
 
 print(df[2019])
 
-'''@router.get("/filter-data")
+@router.get("/filter-data")
 async def filter_data(year:int = Query(...,description = "Year to filter dates")):
     if year not in df:
         return {"Year not found"}
     
     dataframe = df[year]
-    return dataframe.to_dict(orient="records")'''
+    return dataframe.to_dict(orient="records")
