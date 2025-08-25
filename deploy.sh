b279652f41af7c65c899d8ee070a109679ed9ed0
ArdenDiago <diagoarden@gmail.com>
2025-08-25 11:19:49 +0530

    update the port

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ echo "Starting the Server"
 cd backend
 
 echo "Running Server Command"
-gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --workers 2
+gunicorn -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:10000 --workers 2
